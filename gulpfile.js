f8421b894018b6ee377e460053301c1bb6b81d2a
Roman <rozario91208@gmail.com>
2019-04-14 21:02:47 +0300

    strong block

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 
-var gulp          = require('gulp'),
-		gutil         = require('gulp-util' ),
-		sass          = require('gulp-sass'),
-		browserSync   = require('browser-sync'),
-		concat        = require('gulp-concat'),
-		uglify        = require('gulp-uglify'),
-		cleancss      = require('gulp-clean-css'),
-		rename        = require('gulp-rename'),
-		autoprefixer  = require('gulp-autoprefixer'),
-		notify        = require("gulp-notify"); // Errors
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	cleancss = require('gulp-clean-css'),
+	rename = require('gulp-rename'),
+	autoprefixer = require('gulp-autoprefixer'),
+	notify = require("gulp-notify"); // Errors
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
 	browserSync({
 		server: {
 			baseDir: 'app'
@@ -19,30 +19,31 @@ gulp.task('browser-sync', function() {
 	})
 });
 
-gulp.task('scss', function() {
+gulp.task('scss', function () {
 	return gulp.src('app/scss/**/*.scss')
-	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
-	.pipe(rename({ suffix: '.min', prefix : '' }))
-	.pipe(autoprefixer(['last 15 versions']))
-	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.stream())
+		.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+		.pipe(rename({ suffix: '.min', prefix: '' }))
+		.pipe(autoprefixer(['last 15 versions']))
+		.pipe(cleancss({ level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.stream())
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-        // All JS files add here...
+		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+		// All JS files add here...
 		'app/js/common.js', // Always at the end
-		])
-	.pipe(concat('scripts.min.js')) // Concat js files
-	// .pipe(uglify()) // Mifify js
-	.pipe(gulp.dest('app/js'))
-	.pipe(browserSync.reload({ stream: true }))
+	])
+		.pipe(concat('scripts.min.js')) // Concat js files
+		// .pipe(uglify()) // Mifify js
+		.pipe(gulp.dest('app/js'))
+		.pipe(browserSync.reload({ stream: true }))
 });
 
 
-gulp.task('watch', ['scss', 'js', 'browser-sync'], function() {
+gulp.task('watch', ['scss', 'js', 'browser-sync'], function () {
 	gulp.watch('app/scss/**/*.scss', ['scss']);
 	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
 	gulp.watch('app/*.html', browserSync.reload)
